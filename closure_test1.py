3b0becbfed531459afd515b28ddf09effd75c9a5
electrocoding <electrocoding@gmail.com>
2017-07-28 15:12:28 -0700

    Added Comments on 7/28/17

---
+++ closure_test1.py
@@ -0,0 +1,31 @@
+#Closures
+#This is to see how closures work
+##
+
+print("What, No changes?")
+
+
+def outer_func():
+    message = 'hi'
+
+    def inner_func():
+        print(message)
+
+    return inner_func
+
+outer_func()
+print(dir())
+
+print(outer_func())
+print(outer_func)
+
+
+
+#print(my_func.__name__)
+#inner_func()
+
+#my_func = outer_func()
+
+#print(my_func)
+#print(message)
+#print(inner_func)
